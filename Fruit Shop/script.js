1a99992fd9ef34521680cc909ab1a075cb08e0e1
shubhpatel32 <sdpatel972@gmail.com>
2024-03-12 01:30:19 +0530

    FIles added

---
+++ Fruit Shop/script.js
@@ -0,0 +1,66 @@
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  cart.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let cart = document.querySelector(".shopping-cart");
+
+document.querySelector("#cart-btn").onclick = () => {
+  cart.classList.toggle("active");
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let loginForm = document.querySelector(".login-form");
+
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+  searchForm.classList.remove("active");
+  cart.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  cart.classList.remove("active");
+  loginForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  searchForm.classList.remove("active");
+  cart.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let slides = document.querySelectorAll(".home .slides-container .slide");
+let index = 0;
+next();
+
+function next() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+  setTimeout(next, 2000);
+}
+
+let dott = document.querySelectorAll(".home .dot i");
+let index2 = 0;
+next2();
+
+function next2() {
+  dott[index2].classList.remove("active");
+  index2 = (index2 + 1) % dott.length;
+  dott[index2].classList.add("active");
+  setTimeout(next2, 2000);
+}
+
+
